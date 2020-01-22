2238b785d6d2843dd4ac19f13cc58258e42bad6f
stanley-c-so <45887253+stanley-c-so@users.noreply.github.com>
2020-01-22 13:01:14 -0500

    added placeholder button

--- client/app.js
+++ client/app.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 
 const App = () => {
+  const handleClick = () => {
+
+  };
+
   return (
     <div>
       THIS IS APP.JS!
+      <button onClick={handleClick}>CLICK ME</button>
     </div>
   );
 };
