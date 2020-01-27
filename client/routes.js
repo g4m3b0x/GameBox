14804b069ae572763f1d67596c4aee0634ecc7db
Wilson Leung <wilsonleung32@gmail.com>
2020-01-27 15:34:15 -0500

    added start game button

--- client/routes.js
+++ client/routes.js
@@ -8,17 +8,19 @@ class Routes extends Component {
   constructor() {
     super();
     this.state = {
+      userId: null,
       userName: null,
-      roomData: null,
+      roomData: null
     };
   }
 
   componentDidMount() {
     socket.on('joined room', data => {
-      const {userName, roomData} = data;
+      const { userName, roomData, userId } = data;
       this.setState({
+        userId,
         userName,
-        roomData,       // object from server memory
+        roomData // object from server memory
       });
     });
   }
@@ -27,19 +29,18 @@ class Routes extends Component {
     return (
       <div id="middle">
         <div id="dynamic-area">
-        {
-          this.state.roomData === null ? (
+          {this.state.roomData === null ? (
             <Welcome />
           ) : (
             <Lobby
+              userId={this.state.userId}
               userName={this.state.userName}
               roomData={this.state.roomData}
             />
-          )
-        }
+          )}
         </div>
       </div>
-    )
+    );
   }
 }
 
