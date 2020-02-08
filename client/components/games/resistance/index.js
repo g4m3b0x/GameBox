6f6f6d25b57ae47ebe8e89929a528731dc7fdafc
stanley-c-so <45887253+stanley-c-so@users.noreply.github.com>
2020-02-07 19:53:33 -0500

    changed some UI stuff in chat

--- client/components/games/resistance/index.js
+++ client/components/games/resistance/index.js
@@ -66,7 +66,7 @@ export default class Resistance extends Component {
               <div className="flip-card" style={style.flipCard}>
                 <div className="flip-card-inner">
                   <div className="flip-card-front">
-                    <img style={style.card} src="resistance_back.png" />
+                    <img style={style.card} src="resistance_char_back.png" />
                   </div>
                   <div className="flip-card-back">
                     <img
